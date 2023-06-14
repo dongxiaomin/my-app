1eeeba2909c24262d41ceae6653d635c2e0faa75
dxm <1344091359@qq.com>
2023-06-14 10:42:39 +0800

    0613

--- zhihu/src/index.js
+++ zhihu/src/index.js
@@ -7,8 +7,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <div>zhihu</div>
   <>
-    <DemoOne title='我是标题' x={1}/>
-    <DemoOne title='我是标题2' />
+    <DemoOne title='我是标题1' x={1}>
+      <div solt='footer'>2</div>
+      <div solt='header'>1</div>
+    </DemoOne>
+    {/* <DemoOne title='我是标题2'>
+      <div>1</div>
+    </DemoOne>
+    <DemoOne title='我是标题3'>
+    </DemoOne> */}
   </>
 );
 
